ea0c93c86b92071a2ff0151478e594a352a058cc
snehalsjuvekar <snehaljuvekar0@gmail.com>
2026-04-13 09:23:45 +0530

    Initial Selenium Maven project working

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -3,6 +3,7 @@ package com.example;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -11,19 +12,30 @@ public class AppTest {
     @Test
     void testGoogleTitle() {
 
-        WebDriver driver = new ChromeDriver();
+        // Headless setup for Jenkins
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--disable-gpu");
+        options.addArguments("--window-size=1920,1080");
 
-        // Open Google
-        driver.get("https://www.google.com");
+        WebDriver driver = new ChromeDriver(options);
 
-        // Get title
-        String title = driver.getTitle();
-        System.out.println("Page Title: " + title);
+        try {
+            // Open Google
+            driver.get("https://www.google.com");
 
-        // Assertion
-        assertTrue(title.contains("Google"));
+            // Get title
+            String title = driver.getTitle();
+            System.out.println("Page Title: " + title);
 
-        // Close browser
-        driver.quit();
+            // Assertion
+            assertTrue(title.contains("Google"));
+
+        } finally {
+            // Always close browser
+            driver.quit();
+        }
     }
 }
