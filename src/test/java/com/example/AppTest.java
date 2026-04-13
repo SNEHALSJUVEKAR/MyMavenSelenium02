7870d2527386217c534cd546351c7081d0bd062b
snehalsjuvekar <snehaljuvekar0@gmail.com>
2026-04-13 08:56:52 +0530

    Initial Selenium Maven project working

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -1,38 +1,29 @@
 package com.example;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+import static org.junit.jupiter.api.Assertions.*;
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+public class AppTest {
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+    @Test
+    void testGoogleTitle() {
+
+        WebDriver driver = new ChromeDriver();
+
+        // Open Google
+        driver.get("https://www.google.com");
+
+        // Get title
+        String title = driver.getTitle();
+        System.out.println("Page Title: " + title);
+
+        // Assertion
+        assertTrue(title.contains("Google"));
+
+        // Close browser
+        driver.quit();
     }
 }
